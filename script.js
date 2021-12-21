350d677a5c074efd60eec03cddff12bfa00e6e4e
codewithmary23 <92807654+codewithmary23@users.noreply.github.com>
2021-12-21 17:42:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const questions = document.getElementsByClassName('question');
+
+for (i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+
+        let answer = this.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+}
